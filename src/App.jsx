4ccf0f9b738d7246cd5208c470e28c6fd731b032
Misael <misaelbolainez@gmail.com>
2023-05-24 16:50:46 -0700

    feat(router + Context): Set up the router for the pages and the intial context

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import './App.css'
+import Header from "./components/Header"
+import Cart from "./pages/Cart"
+import Photos from "./pages/Photos"
+import {Routes, Route} from "react-router-dom"
 
 function App() {
 
   return (
     <div>
-      
+      <Header />
+      <Routes>
+        <Route path = "/" element={<Photos/>}></Route>
+        <Route path = "/cart" element={<Cart/>}></Route>
+      </Routes>
     </div>
   )
 }
