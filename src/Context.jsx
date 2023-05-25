db9a5673eeb453d78d562de85b05134227e8e4d5
Misael Bolainez <misaelbolainez@gmail.com>
2023-05-25 15:43:06 -0700

    feat(fetch): Added fetch data from url

--- src/Context.jsx
+++ src/Context.jsx
@@ -3,8 +3,16 @@ import React from "react"
 const Context = React.createContext()
 
 function ContextProvider({children}) {
+    const [photos, setPhotos] = React.useState([]);
+
+    React.useEffect(() => {
+        fetch("https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json")
+            .then(response => response.json())
+            .then(data => setPhotos(data))
+    },[]);
+
     return (
-        <Context.Provider value="">
+        <Context.Provider value={{photos}}>
             {children}
         </Context.Provider>
     )
